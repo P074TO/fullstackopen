090672fc455446027123e0e29809f1f0b90eaf90
Jr Rob Saluday <jrrobsaluday26@gmail.com>
2025-05-01 23:00:33 +0800

    feat: finish exercise 2.4

---
+++ part2/courseinfo/2.4/src/App.jsx
@@ -0,0 +1,94 @@
+const Header = ({ header }) => {
+  return <h1>{header}</h1>;
+};
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  );
+};
+
+const Total = ({ sum }) => {
+  return <b>total of {sum} exercises</b>;
+};
+
+const Course = ({ course }) => {
+  const sum = course.parts.reduce((sum, part) => sum + part.exercises, 0);
+  return (
+    <>
+      <Header header={course.name} />
+      {course.parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+      <Total sum={sum} />
+    </>
+  );
+};
+
+const Courses = ({ courses }) => {
+  return (
+    <>
+      <Header header="Web development curriculum" />
+      {courses.map((course) => (
+        <Course key={course.id} course={course} />
+      ))}
+    </>
+  );
+};
+
+const App = () => {
+  const courses = [
+    {
+      name: "Half Stack application development",
+      id: 1,
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: "Redux",
+          exercises: 11,
+          id: 4,
+        },
+      ],
+    },
+    {
+      name: "Node.js",
+      id: 2,
+      parts: [
+        {
+          name: "Routing",
+          exercises: 3,
+          id: 1,
+        },
+        {
+          name: "Middlewares",
+          exercises: 7,
+          id: 2,
+        },
+      ],
+    },
+  ];
+
+  return (
+    <div>
+      <Courses courses={courses} />
+    </div>
+  );
+};
+
+export default App;
