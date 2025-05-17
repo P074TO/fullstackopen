5c8db658a1ed104271d550356d0ca58a9d881542
Jr Rob Saluday <jrrobsaluday26@gmail.com>
2025-05-17 15:48:35 +0800

    feat: finish ex2.7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
 
-const Name = ({ name }) => <p>{name}</p>;
+const Person = ({ person }) => (
+  <p>
+    {person.name} {person.number}
+  </p>
+);
 
 const Persons = ({ persons }) => (
   <>
     {persons.map((person) => (
-      <Name key={person.id} name={person.name} />
+      <Person key={person.id} person={person} />
     ))}
   </>
 );
 
 const App = () => {
-  const [persons, setPersons] = useState([{ id: "1", name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const addPerson = (event) => {
     event.preventDefault();
     const personObject = {
       name: newName,
+      number: newNumber,
       id: String(persons.length + 1),
     };
 
@@ -32,10 +43,15 @@ const App = () => {
     }
 
     setNewName("");
+    setNewNumber("");
   };
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
   };
 
   return (
@@ -44,6 +60,9 @@ const App = () => {
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
