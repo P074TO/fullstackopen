9da8c69d0ae1d34f8916f06df27192196cf9f2ac
Jr Rob Saluday <jrrobsaluday26@gmail.com>
2025-05-17 15:05:25 +0800

    feat: finish exercise 2.5

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,36 +1,8 @@
-const Header = ({ name }) => <h1>{name}</h1>;
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-);
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </>
-);
-
-const Total = ({ parts }) => (
-  <b>
-    total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
-  </b>
-);
-
-const Course = ({ course }) => (
-  <>
-    <Header name={course.name} />
-    <Content parts={course.parts} />
-    <Total parts={course.parts} />
-  </>
-);
+import Course from "./components/Course.jsx";
 
 const Courses = ({ courses }) => (
   <>
-    <Header name="Web development curriculum" />
+    <h1>Web development curriculum</h1>
     {courses.map((course) => (
       <Course key={course.id} course={course} />
     ))}
