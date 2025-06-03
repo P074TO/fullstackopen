f6f0246a0aab2f3ce7c7f7613dfcfbbddc7bca7c
Jr Rob Saluday <jrrobsaluday26@gmail.com>
2025-06-03 16:51:04 +0800

    jhfdasbhfdafda

---
+++ part3/phonebook/backend/mongo.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+const inputName = process.argv[3];
+const inputNumber = process.argv[4];
+
+const url = `mongodb+srv://jrrobsaluday26:${password}@cluster0.ymmpgof.mongodb.net/personApp?retryWrites=true&w=majority&appName=Cluster0`;
+
+mongoose.set("strictQuery", false);
+
+mongoose.connect(url);
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: inputName,
+  number: inputNumber,
+});
+
+if (person.name && person.number) {
+  person.save().then((result) => {
+    console.log(`added ${person.name} number ${person.number} to phonebook`);
+    mongoose.connection.close();
+  });
+} else {
+  Person.find({}).then((result) => {
+    console.log("phonebook:");
+    result.forEach((person) => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
